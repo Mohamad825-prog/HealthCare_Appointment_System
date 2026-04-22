c072b7c9d2f2c53312e66d51c501f05e5bcb2293
Mohamad825-prog <mhd.kh1272005@gmail.com>
2026-04-22 20:24:30 +0300

    Implement API routes for users doctors services and appointments

---
+++ backend/routes/doctorRouter.js
@@ -0,0 +1,24 @@
+import express from "express";
+import multer from "multer";
+
+import { createDoctor, deleteDoctor, getDoctors, toggleAvailability, updateDoctor } from '../controllers/doctorController.js';
+import { doctorLogin, getDoctorById } from '../controllers/doctorController.js';
+
+import doctorAuth from '../middlewares/doctorAuth.js';
+
+const upload = multer({ dest: "/tmp" }); // temporary storage for uploaded files
+
+const doctorRouter = express.Router();
+
+doctorRouter.get("/", getDoctors);
+doctorRouter.post('/login', doctorLogin);
+
+doctorRouter.get("/:id", getDoctorById);
+doctorRouter.post("/", upload.single("image"), createDoctor);
+
+// After login
+doctorRouter.put("/:id", doctorAuth, upload.single("image"), updateDoctor);
+doctorRouter.post("/:id/toggle-availability", doctorAuth, toggleAvailability);
+doctorRouter.delete("/:id", deleteDoctor);
+
+export default doctorRouter;
