19421a30ecd4817caf3dcf5f2f5a5b94e4f72e97
Mohamad825-prog <mhd.kh1272005@gmail.com>
2026-04-26 17:50:50 +0300

    Implement service management in admin dashboard

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Hero from './pages/Hero'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Hero from './pages/Hero';
 import { useUser } from "@clerk/clerk-react";
 import { Link } from 'react-router-dom';
 import Home from './pages/Home';
